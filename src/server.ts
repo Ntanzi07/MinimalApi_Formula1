bf236e303254ab5e148d101bb8b5682cedf54314
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-01-29 14:53:14 -0300

    Creating Data

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,21 @@
 import fastify from "fastify";
+import * as teamsJson from "./data/teams.json"
+import * as driversJson from "./data/drivers.json"
+
+const teams = teamsJson;
+const drivers = driversJson;
 
 const server = fastify({ logger: true });
 
 server.get("/teams", async (request, response) => {
     response.type("application/json").code(200);
-    return [{
-        id: 1,
-        name: "Max Verstappen",
-        team: "Red Bull Racing",
-        championships: 3
-    }];
+    return teams;
 });
+server.get("/drivers", async (request, response) => {
+    response.type("application/json").code(200);
+    return drivers;
+})
 
-server.listen({port: 3333}, () => {
+server.listen({ port: 3333 }, () => {
     console.log("Server init");
 })
