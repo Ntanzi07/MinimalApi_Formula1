0a83075190731ad363c180a54ca1f3268737f231
Nathan Tanzi <nathan.tanzi12345@gmail.com>
2025-01-29 15:06:31 -0300

    filter Driver

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,28 @@ server.get("/teams", async (request, response) => {
     response.type("application/json").code(200);
     return teams;
 });
+
 server.get("/drivers", async (request, response) => {
     response.type("application/json").code(200);
     return drivers;
+})
+
+interface DriversParams {
+    id: string
+}
+
+server.get<{ Params: DriversParams }>("/drivers/:id", async (request, response) => {
+    const id = parseInt(request.params.id);
+    const drive = drivers.find(d => d.id === id)
+
+    if (!drive) {
+        response.type("application/json").code(404);
+        return { message: "Drive not founded :(" };
+    } else {
+        response.type("application/json").code(200);
+        return drive;
+    }
+
 })
 
 server.listen({ port: 3333 }, () => {
